2ab7d6af3f8d754b63e3116af9d6f220202937f4
kowalczyk-mateusz <kowalczyk_mateusz@poczta.fm>
2021-03-23 22:55:45 +0100

    Adding script using TenserFLow.js and coco-ssd model

---
+++ script.js
@@ -0,0 +1,55 @@
+var modelHasLoaded = false;
+var model = undefined;
+
+cocoSsd.load().then(function(loadedModel){
+    model = loadedModel;
+	modelHasLoaded = true;
+});
+
+const holderOfImage = document.getElementsByClassName('imageContainer');
+
+for(let i = 0; i<holderOfImage.length; i++){
+    holderOfImage[i].children[0].addEventListener('click', handleClick);
+}
+
+function handleClick(e){
+    if(!modelHasLoaded){
+        return;
+    }
+    model.detect(e.target).then(function(predictions){
+        for(let i = 0; i < predictions.length; i++){
+            const p = document.createElement('p');
+            p.innerText =
+             predictions[i].class +
+            ' - with ' +
+            Math.round(parseFloat(predictions[i].score) * 100) +
+            '% of confidence.';
+            p.style =
+             'margin-left: ' +
+            predictions[i].bbox[0] + 
+            'px; margin-top: ' +
+            (predictions[i].bbox[1] - 10) + 
+            'px; width: ' + 
+            (predictions[i].bbox[2] - 10) + 
+            'px; top: 0; left: 0;';
+
+            const objectBorder  = document.createElement('div');
+            objectBorder .setAttribute('class', 'objectBorder');
+            objectBorder .style = 
+            'left: ' +
+            predictions[i].bbox[0] +
+            'px; top: ' +
+            predictions[i].bbox[1] +
+            'px; width: ' + 
+            predictions[i].bbox[2] + 
+            'px; height: ' +
+            predictions[i].bbox[3] +
+            'px;';
+
+
+            e.target.parentNode.appendChild(objectBorder );
+            e.target.parentNode.appendChild(p);
+        }
+    })
+}
+
